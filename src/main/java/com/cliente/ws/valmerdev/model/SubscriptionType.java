20480dcc17be63afa2adaf1372c1ffe74b073ef7
hvalmer <hvalmer@gmail.com>
2025-07-30 17:18:54 -0300

    running migrations, creating entities, using lombok

---
+++ src/main/java/com/cliente/ws/valmerdev/model/SubscriptionType.java
@@ -0,0 +1,36 @@
+package com.cliente.ws.valmerdev.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "subscriptions_type")
+public class SubscriptionType implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "subscriptions_type_id")
+    private Long id;
+
+    private String name;
+
+    @Column(name = "access_months")
+    private Long accessMonth;
+
+    private BigDecimal price;
+
+    @Column(name = "product_key")
+    private String productkey;
+
+}
